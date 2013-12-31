ba2352192013101b49b279b98ed66f2d9463be0c
Ian <ian.h.grant@gmail.com>
2013-12-30 21:22:18 -0500

    fixes!

--- client/js/ui.js
+++ client/js/ui.js
@@ -2,9 +2,29 @@
 
 $(document).ready(function () {
 	Sasquatch.loadCards().done(function () {
-		console.log('done loading cards');
+		var cards = Sasquatch.getCards(),
+			temp = getTemplate('card'),
+			out = '';
+		$.each(cards, function (i, el) {
+			out += temp.replace(/#name/g, el.name)
+				.replace(/#type/g, el.type)
+				.replace(/#description/g, el.description);
+		});
+		$('#board').html(out);
+	}).fail(function () {
+		console.log('failure');
 	});
 });
+
+var getTemplate = function (id) {
+	switch (id) {
+		case 'card':
+			return '<div class="card" draggable="true" data-type="#type"><div class="cardinterior"><h1>#name</h1><p>#description</p></div></div>';
+		default:
+			console.log('I don\'t recognise that template.');
+			break;
+	}
+};
 
 var Sasquatch = (function () {
 	var cards = {};
@@ -14,6 +34,10 @@ var Sasquatch = (function () {
 			type: 'GET',
 			url: target,
 			dataType: 'json'
+		}).done(function (data) {
+			dfd.resolve(data);
+		}).fail(function (jqXHR, textStatus, errorThrown) {
+			dfd.reject('Failure.', jqXHR);
 		});
 		return dfd.promise();
 	};
@@ -21,15 +45,19 @@ var Sasquatch = (function () {
 		var dfd = $.Deferred();
 		getData('cards.json').done(function (data) {
 			cards = data;
+			console.log(cards, data);
 			dfd.resolve();
 		}).fail(function (msg) {
 			dfd.reject(msg);
 		});
 		return dfd.promise();
 	};
+	var getCards = function () {
+		return cards;
+	};
 
 	return {
-		cards: cards,
+		getCards: getCards,
 		loadCards: loadCards
 	};
 })(this);
